0db3309471df2f99a246cb2b934b124fbe07796c
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-05-16 20:32:35 +0200

    feat: add footer

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,9 @@
 import '../scss/style.scss';
 import 'bootstrap';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const yearElement = document.getElementById('year');
+  if (yearElement) {
+    yearElement.textContent = new Date().getFullYear().toString();
+  }
+});
